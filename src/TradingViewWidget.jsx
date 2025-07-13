28228954cd927361fd04e1258bb0c0781a3f606a
Juan Jose <cererbrotrading@gmail.com>
2025-07-13 12:27:51 -0500

    Update TradingViewWidget.jsx

--- src/TradingViewWidget.jsx
+++ src/TradingViewWidget.jsx
@@ -1,22 +1,22 @@
-// src/TradingViewWidget.jsx
 import React from 'react';
+import { AdvancedChart } from 'react-tradingview-embed';
 
-function TradingViewWidget({ symbol }) {
-  const src = `https://s.tradingview.com/embed-widget/symbol-overview/?locale=en#%7B%22symbols%22%3A%5B%5B%22NASDAQ%3A${symbol}%22%5D%5D%2C%22width%22%3A%22100%25%22%2C%22height%22%3A250%2C%22isTransparent%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22autosize%22%3Atrue%7D`;
-
+const TradingViewWidget = ({ symbol }) => {
   return (
-    <div style={{ flex: 1, padding: '10px' }}>
-      <iframe
-        src={src}
-        width="100%"
-        height="250"
-        frameBorder="0"
-        allowTransparency="true"
-        scrolling="no"
-        title={`TradingView ${symbol}`}
-      ></iframe>
+    <div className="w-full h-[500px]">
+      <AdvancedChart
+        widgetProps={{
+          symbol: symbol,
+          interval: '1',
+          theme: 'dark',
+          style: '1', // 1 = candle chart
+          locale: 'en',
+          width: '100%',
+          height: '100%',
+        }}
+      />
     </div>
   );
-}
+};
 
 export default TradingViewWidget;
